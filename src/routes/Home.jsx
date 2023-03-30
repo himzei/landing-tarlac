5996fb91a67087c7c8894c69cb275838d40f6ca1
조현일 <himzei@gmail.com>
2023-03-30 13:53:08 +0900

    init

---
+++ src/routes/Home.jsx
@@ -0,0 +1,39 @@
+import { Text, VStack } from "@chakra-ui/react";
+import { Element } from "react-scroll";
+
+export default function Home() {
+  return (
+    <>
+      {/* 메인 슬라이드 */}
+      <VStack w="full" h="100vh" justifyContent="center">
+        <Text fontWeight={600} fontSize="40">
+          제작중입니다.
+        </Text>
+      </VStack>
+
+      <div id="1">
+        <Element name="test1" className="test1"></Element>
+      </div>
+
+      <div id="2">
+        <Element name="test2" className="test2"></Element>
+      </div>
+
+      <div id="3">
+        <Element name="test3" className="test3"></Element>
+      </div>
+
+      <div id="4">
+        <Element name="test4" className="test4"></Element>
+      </div>
+
+      <div id="5">
+        <Element name="test5" className="test5"></Element>
+      </div>
+
+      <div id="6">
+        <Element name="test6" className="test6"></Element>
+      </div>
+    </>
+  );
+}
